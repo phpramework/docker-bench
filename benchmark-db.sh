d8ea94d934edfe448df8b9180e195a99d9c6566d
phpramework <phpramework@gmail.com>
2016-10-30 21:03:27 +0700

    Remove slash because our uri param already had it.

--- benchmark-db.sh
+++ benchmark-db.sh
@@ -3,21 +3,21 @@
 echo ""
 echo "---------------------------------------------------------"
 echo " Benchmarking ${FRAMEWORK}: Single Query : Primer"
-echo " wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server/${URI_DB}"
+echo " wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server${URI_DB}"
 echo "---------------------------------------------------------"
 echo ""
 
-wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server/${URI_DB}
+wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server${URI_DB}
 sleep 5
 
 echo ""
 echo "---------------------------------------------------------"
 echo " Benchmarking ${FRAMEWORK}: Single Query : Warming Up"
-echo " wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server/${URI_DB}"
+echo " wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server${URI_DB}"
 echo "---------------------------------------------------------"
 echo ""
 
-wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server/${URI_DB}
+wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server${URI_DB}
 sleep 5
 
 for c in 8 16 32 64 128 256
@@ -25,10 +25,10 @@ do
     echo ""
     echo "---------------------------------------------------------"
     echo " Benchmarking ${FRAMEWORK}: Single Query : Concurency $c"
-    echo " wrk --latency -d 15 -c $c -t ${BENCHMARK_CORES} http://app-server/${URI_DB}"
+    echo " wrk --latency -d 15 -c $c -t ${BENCHMARK_CORES} http://app-server${URI_DB}"
     echo "---------------------------------------------------------"
     echo ""
 
-    wrk --latency -d 15 -c $c -t ${BENCHMARK_CORES} http://app-server/${URI_DB} 2>&1 | tee /result/db_$c.output
+    wrk --latency -d 15 -c $c -t ${BENCHMARK_CORES} http://app-server${URI_DB} 2>&1 | tee /result/db_$c.output
     sleep 5
 done
--- benchmark-json.sh
+++ benchmark-json.sh
@@ -3,21 +3,21 @@
 echo ""
 echo "---------------------------------------------------------"
 echo " Benchmarking ${FRAMEWORK}: JSON Encoding : Primer"
-echo " wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server/${URI_JSON}"
+echo " wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server${URI_JSON}"
 echo "---------------------------------------------------------"
 echo ""
 
-wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server/${URI_JSON}
+wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server${URI_JSON}
 sleep 5
 
 echo ""
 echo "---------------------------------------------------------"
 echo " Benchmarking ${FRAMEWORK}: JSON Encoding : Warming Up"
-echo " wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server/${URI_JSON}"
+echo " wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server${URI_JSON}"
 echo "---------------------------------------------------------"
 echo ""
 
-wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server/${URI_JSON}
+wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server${URI_JSON}
 sleep 5
 
 for c in 8 16 32 64 128 256
@@ -25,10 +25,10 @@ do
     echo ""
     echo "---------------------------------------------------------"
     echo " Benchmarking ${FRAMEWORK}: JSON Encoding : Concurency $c"
-    echo " wrk --latency -d 15 -c $c -t ${BENCHMARK_CORES} http://app-server/${URI_JSON}"
+    echo " wrk --latency -d 15 -c $c -t ${BENCHMARK_CORES} http://app-server${URI_JSON}"
     echo "---------------------------------------------------------"
     echo ""
 
-    wrk --latency -d 15 -c $c -t ${BENCHMARK_CORES} http://app-server/${URI_JSON} 2>&1 | tee /result/json_$c.output
+    wrk --latency -d 15 -c $c -t ${BENCHMARK_CORES} http://app-server${URI_JSON} 2>&1 | tee /result/json_$c.output
     sleep 5
 done
