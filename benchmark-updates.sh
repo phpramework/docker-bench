1fe89c0e9a7c5c7fff174429ff1de2aa4f314d92
phpramework <phpramework@gmail.com>
2016-10-31 11:02:12 +0700

    Forgot to add params.

--- benchmark-updates.sh
+++ benchmark-updates.sh
@@ -3,21 +3,21 @@
 echo ""
 echo "---------------------------------------------------------"
 echo " Benchmarking ${FRAMEWORK}: DB Updates : Primer"
-echo " wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server${URI_UPDATES}"
+echo " wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server${URI_UPDATES}20"
 echo "---------------------------------------------------------"
 echo ""
 
-wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server${URI_UPDATES}
+wrk --latency -d 5 -c 8 -t ${BENCHMARK_CORES} http://app-server${URI_UPDATES}20
 sleep 5
 
 echo ""
 echo "---------------------------------------------------------"
 echo " Benchmarking ${FRAMEWORK}: DB Updates : Warming Up"
-echo " wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server${URI_UPDATES}"
+echo " wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server${URI_UPDATES}20"
 echo "---------------------------------------------------------"
 echo ""
 
-wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server${URI_UPDATES}
+wrk --latency -d 15 -c 256 -t ${BENCHMARK_CORES} http://app-server${URI_UPDATES}20
 sleep 5
 
 for c in 1 5 10 15 20
